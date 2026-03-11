a91a877badf62d81e900277cd75a4acbfa20a956
Jon LeVee <jonlevee@gmail.com>
2026-03-11 16:56:06 -0700

    update

---
+++ NPCChatLib/Builders/TownTemplateFactory.cs
@@ -0,0 +1,33 @@
+﻿using NPCChatLib.Attributes;
+
+namespace NPCChatLib.Builders
+{
+    public enum TownTemplateName
+    {
+        StandardB3H3C5,
+    }
+
+    [Singleton]
+    public class TownTemplateFactory
+    {
+        private readonly TownTemplate[] _templates =
+            [
+                new TownTemplate(
+                    TownTemplateName.StandardB3H3C5,
+                    )
+            ];
+        public TownTemplateFactory()
+        {
+        }
+    }
+
+    public class TownTemplate
+    {
+        public TownTemplateName Name { get; }
+
+        public TownTemplate(TownTemplateName name)
+        {
+            Name = name;
+        }
+    }
+}
